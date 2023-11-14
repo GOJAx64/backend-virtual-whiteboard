c3940771a5c64ef2c8091d486f83df9207c2c63c
Alberto Garcia <josealbertogarcia453@gmail.com>
2023-11-14 12:47:27 -0600

    returning to functional app

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 import { Sequelize } from 'sequelize';
 
-
 const db = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    // port: 3307,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    ssl: true,
     pool: {
         max: 10,
         min: 0,
